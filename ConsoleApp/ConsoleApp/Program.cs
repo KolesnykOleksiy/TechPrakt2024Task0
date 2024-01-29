19988a81d86112bfcef8dec634dd43cebee359a5
unknown <vt222_koa@student.ztu.edu.ua>
2024-01-29 11:33:07 +0200

    Створено консольний додаток, додано файл  .gitignore

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть перше число:");
+        if (double.TryParse(Console.ReadLine(), out double a))
+        {
+
+            Console.WriteLine("Введіть друге число:");
+            if (double.TryParse(Console.ReadLine(), out double b))
+            {
+
+                double sum = a + b;
+
+                Console.WriteLine($"Сума чисел {a} + {b} дорівнює {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Помилка введення для другого числа");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Помилка введення для першого числа");
+        }
+
+        Console.ReadLine();
+    }
+
+}
